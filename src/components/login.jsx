95f32ca84c481634103e48d5de34faed83abdbc8
laridev <laridev@gmail.com>
2019-07-26 23:51:04 +0100

    login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,40 @@
 import React from "react";
 import styled from "styled-components";
-
+import login from "../images/login.jpg";
 export default function Login() {
-  return <div>hello world</div>;
+  return (
+    <div className="uk-container">
+      <div className="uk-child-width-1-2 uk-text-center" uk-grid>
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">
+            <form>
+              <div className="uk-margin uk-width-1-1">
+                <div className="uk-inline">
+                  <span className="uk-form-icon" uk-icon="icon: user" />
+                  <input
+                    className="uk-input"
+                    type="text"
+                    placeholder="User name"
+                  />
+                </div>
+              </div>
+              <div className="uk-margin uk-width-1-1">
+                <div className="uk-inline">
+                  <span className="uk-form-icon" uk-icon="icon: lock" />
+                  <input
+                    className="uk-input"
+                    type="password"
+                    placeholder="password"
+                  />
+                </div>
+              </div>
+              <div className="uk-margin uk-width-1-1">
+                <button className="uk-button uk-button-primary">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
