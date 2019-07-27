ff398556f979cd0562da0a8d167ce317e19d56ad
laridev <laridev@gmail.com>
2019-07-27 23:23:13 +0100

    adding board component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-
+import styled from 'styled-components';
 class Board extends Component {
+ 
     render() {
-        return (
-            <div>
-                
-            </div>
+      const MYBOARD = styled.div`
+        border-radius: 4%;
+        cursor : pointer;
+        padding : 12px 6px;
+        height : 140px;
+        background : ${this.props.color} url(${this.props.image});
+        background-size : cover;
+     `;
+     const TITLE  = styled.h3`
+        font-weight: 500;
+     
+     `;
+       return (
+        <MYBOARD className="uk-card uk-card-default uk-card-body uk-card-hover">
+            <TITLE className="uk-card-title">arc gis learning</TITLE>
+          </MYBOARD>
         );
     }
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import LoginImage from "../images/login.jpg"
+import LoginImage from "../images/login.jpg";
+
+import Board from "./Board";
 class Main extends Component {
   render() {
     return (
@@ -9,73 +11,72 @@ class Main extends Component {
             <span uk-icon="star" /> Starred Boards
           </h4>
           <div
-            class="uk-child-width-1-5@m uk-grid-small uk-grid-match"
+            class="uk-child-width-1-4@m uk-grid-small uk-grid-match"
             data-uk-grid="true"
           >
             <div>
-            <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" style={{backgroundImage: `url(${LoginImage})`}}>
-                <h3 class="uk-card-title">arcgis Learning</h3>
-              </div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
             </div>
             <div>
-              <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" style={{backgroundImage: `url(${LoginImage})`}}>
-                <h3 class="uk-card-title">arcgis Learning</h3>
-              </div>
+              <Board image={LoginImage} color={""} title={""}/>
             </div>
             <div>
-            <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" style={{background: "#1e87f0"}}>
-                <h3 class="uk-card-title">arcgis Learning</h3>
+              <Board image={"LoginImage"} color={"#00bcd4"} title={""}/>
             </div>
+            <div>
+              <Board image={LoginImage} color={""} title={""}/>
             </div>
           </div>
         </div>
-        <hr/>
+        <hr />
         <div className="section-2">
           <h4>
             <span uk-icon="user" /> Personal Boards
           </h4>
-          <div class="uk-child-width-1-5@m uk-grid-small uk-grid-match" data-uk-grid="true" >
-      
-          <div>
-              <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" style={{backgroundImage: `url(${LoginImage})`}}>
-                <h3 class="uk-card-title">arcgis Learning</h3>
-              </div>
+          <div
+            class="uk-child-width-1-4@m uk-grid-small uk-grid-match"
+            data-uk-grid="true"
+          >
+           <div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
             </div>
             <div>
-              <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" style={{backgroundImage: `url(${LoginImage})`}}>
-                <h3 class="uk-card-title">arcgis Learning</h3>
-              </div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
             </div>
             <div>
-              <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" style={{backgroundImage: `url(${LoginImage})`}}>
-                <h3 class="uk-card-title">arcgis Learning</h3>
-              </div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
             </div>
             <div>
-              <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" style={{backgroundImage: `url(${LoginImage})`}}>
-                <h3 class="uk-card-title">arcgis Learning</h3>
-              </div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
             </div>
             <div>
-              <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" style={{backgroundImage: `url(${LoginImage})`}}>
-                <h3 class="uk-card-title">arcgis Learning</h3>
-              </div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
             </div>
-
             <div>
-              <div class="uk-background-cover uk-height-small uk-panel uk-flex uk-flex-center uk-flex-middle" style={{backgroundImage: `url(${LoginImage})`}}>
-                <h3 class="uk-card-title">arcgis Learning</h3>
-              </div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
             </div>
-                    
+            <div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
+            </div>
+            <div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
+            </div>
+            <div>
+              <Board image={LoginImage} color={"#ec2147"} title={""}/>
+            </div>
+            <div>
+              <Board image={""} color={"gray"} title={""}/>
+            </div>          
           </div>
-           <div className="uk-margin">
-            <a href="dd" className="uk-button uk-button-danger">Show More ...</a>
-         </div> 
+          <div className="uk-margin">
+            <a href="dd" className="uk-button uk-button-danger">
+              Show More ...
+            </a>
+          </div>
         </div>
-        <hr/>
+        <hr />
         <div className="section-3">
-        <h4>
+          <h4>
             <span uk-icon="users" /> test***
           </h4>
         </div>
