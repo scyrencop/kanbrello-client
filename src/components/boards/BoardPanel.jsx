8d8fd180f805eb5996dbed704959ed443569bc84
laridev@gmail.com <laridev@gmail.com>
2019-08-04 02:27:58 +0100

    add some board logic

--- src/components/boards/BoardPanel.jsx
+++ src/components/boards/BoardPanel.jsx
@@ -7,61 +7,47 @@ import Board from './Board';
 class BoardPanel extends Component {
   constructor(props) {
     super(props);
-    const { showModal } = this.props;
     this.state = {
-      showModal
+       title : "",
+       teamName : "No-team",
+       image : "",
+       color : "red"
     };
   }
+
+  addBoard = e=>{
+
+  }
+
+  handleTitle = e=> {
+      this.setState({
+          title:  e.target.value
+      })
+  }
+
+  handleSelect = e =>{
+      this.setState({
+          teamName : e.target.value
+      })
+  }
+
 
   render() {
     return (
       <div id="my-id" data-uk-modal="true">
         <div className="uk-modal-dialog uk-modal-body">
-          <button class="uk-modal-close-default" type="button" data-uk-close />
+          <button className="uk-modal-close-default" type="button" data-uk-close />
           <form>
-            <div class=" uk-margin">
-              <input class="uk-input" placeholder="Add board title" />
+            <div className=" uk-margin">
+              <input className="uk-input" placeholder="Add board title" onChange={this.handleTitle} value={this.state.title} />
             </div>
-            <div class="uk-margin uk-inline">
-              <div class="uk-form-controls">
-                <select class="uk-select" id="form-stacked-select">
-                  <option>No team</option>
-                  <option>test</option>
+            <div className="uk-margin uk-inline">
+              <div className="uk-form-controls">
+                <select className="uk-select" id="form-stacked-select" onChange={this.handleSelect}>
+                  <option value="No-team">No team</option>
+                  <option value="team1">test</option>
                 </select>
               </div>
-            </div>
-            <div class="uk-inline">
-              <button class="uk-button uk-button-default" type="button">
-                <span data-uk-icon="lock" /> Private
-              </button>
-              <ul
-                class="uk-nav uk-navbar-dropdown-nav "
-                data-uk-dropdown="mode: click"
-              >
-                <li className="my-description">
-                  <a href="">
-                    <span className="my-description-title">
-                      Create Board...
-                    </span>{" "}
-                    <p className="my-description-text">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Pariatur vero ducimus{" "}
-                    </p>
-                  </a>
-                </li>
-                <hr className="my-dividors" />
-                <li className="my-description">
-                  <a href="">
-                    <span className="my-description-title">
-                      Create Business Team...
-                    </span>{" "}
-                    <p className="my-description-text">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Pariatur vero ducimus{" "}
-                    </p>{" "}
-                  </a>
-                </li>
-              </ul>
             </div>
           </form>
           <hr />
@@ -74,7 +60,9 @@ class BoardPanel extends Component {
                 <Board  
                     image={image}
                     color={'red'}
-                    title={""}/>
+                    title={""}
+                    onClick = {this.handleBoardClick} 
+                    />
               </div>
               <div>
                  <Board  
